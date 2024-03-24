46d819a843ee07f2153986e2cdf8e625b61a8457
mcm3583 <dhanushc.ms@gmail.com>
2024-03-24 18:51:25 +0530

    Initial commit

---
+++ app/src/main/java/com/manju/mainapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.manju.mainapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.manju.mainapp.ui.theme.MainAppTheme
+import com.manju.mylibrary.MainScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                   MainScreen()
+                }
+            }
+        }
+    }
+}
+
